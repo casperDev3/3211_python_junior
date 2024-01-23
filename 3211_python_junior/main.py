71c5c165209b1715418cbbf479351c579e698783
Ihor Lialiuk <93039953+casperDev3@users.noreply.github.com>
2024-01-23 17:20:06 +0200

    Update main.py

--- 3211_python_junior/main.py
+++ 3211_python_junior/main.py
@@ -1,1 +1,34 @@
+# comments
 
+# testnameuser = "test"  # chaotic
+# testNameUser = 'test'  # camel case
+# TestNameUser = "test" # paskal case
+# # test-name-user =  'test' # kebab case
+# test_name_user = "test" # snake case
+
+# TYPES\
+
+#### string (str)
+
+txtOne = "Test Hello World!"
+txtTwo = 'test Hrllelel kfkfk'
+txtThree = """test three"""
+txtFour = '''test four'''
+
+### numbers
+# integer (int)
+numOne = 234
+# float (float)
+numTwo = 23.4
+# complex
+numThree = 21j
+
+### boolean
+bOne = True
+bTwo = False
+
+print(type(txtFour))
+print(type(numOne))
+print(type(numTwo))
+print(type(numThree))
+print(type(bOne))
