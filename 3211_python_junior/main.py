9d7319e7705e18f2e976058a7979aadfd2d25117
Ihor Lialiuk <93039953+casperDev3@users.noreply.github.com>
2024-01-25 17:21:18 +0200

    Update main.py

--- 3211_python_junior/main.py
+++ 3211_python_junior/main.py
@@ -6,29 +6,64 @@
 # # test-name-user =  'test' # kebab case
 # test_name_user = "test" # snake case
 
-# TYPES\
+# TYPES -- Primitives
 
-#### string (str)
+# #### string (str)
+#
+# txtOne = "Test Hello World!"
+# txtTwo = 'test Hrllelel kfkfk'
+# txtThree = """test three"""
+# txtFour = '''test four'''
+#
+# ### numbers
+# # integer (int)
+# numOne = 234
+# # float (float)
+# numTwo = 23.4
+# # complex
+# numThree = 21j
+#
+# ### boolean
+# bOne = True
+# bTwo = False
+#
+# ### None
+# vNone = None
 
-txtOne = "Test Hello World!"
-txtTwo = 'test Hrllelel kfkfk'
-txtThree = """test three"""
-txtFour = '''test four'''
+# # OPERATION with primitives types
+# print("Type function --", type(txtOne))
+# print("UpperCase -- ", txtOne.upper())
+# print("LowerCase -- ", txtOne.lower())
+# print("Capitalize --", txtOne.capitalize())
+# print("Title -- ", txtThree.title())
+# print("Get Letter", txtOne[2])
+#
+# # number
+# print("Plus = ", numTwo + numOne)
+# print("Mimus = ", numTwo - numOne)
+# print("Multi = ", numTwo * numOne)
+# print("Dev = ", numTwo / numOne)
+# print("Up = ", numTwo**2)
+# print("Full = ", numTwo % 2)
+# print("Inc = ", numOne + 1)
+# print("Dec = ", numOne - 1)
+# print("Round = ", round(numTwo / numOne, 2))
 
-### numbers
-# integer (int)
-numOne = 234
-# float (float)
-numTwo = 23.4
-# complex
-numThree = 21j
+# # cond numbers
+# print(numOne >= numTwo)
+# print(numOne > numTwo)
+# print(numOne < numTwo)
+# print(numOne <= numTwo)
+# print(numOne == numTwo)
+# print(numOne != numTwo)
 
-### boolean
-bOne = True
-bTwo = False
+####### --- INPUT DATA --- #######
+# template data John Patrick Doe
+print("Doe J. P.")
+firstName = input("Please enter your firstname:  ").capitalize()
+fathersName = input("Please enter your fathersname:  ").capitalize()
+lastName = input("Please enter your lastname:  ").capitalize()
 
-print(type(txtFour))
-print(type(numOne))
-print(type(numTwo))
-print(type(numThree))
-print(type(bOne))
+# print(lastName + " " + firstName[0] + ". " + fathersName[0] + ".")
+pib = f"{lastName} {firstName[0]}. {fathersName[0]}."
+print(pib)
