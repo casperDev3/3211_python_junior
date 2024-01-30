9db3e25cf8e5dd90c2280d11d132c630abe5dcc1
Ihor Lialiuk <93039953+casperDev3@users.noreply.github.com>
2024-01-30 17:13:34 +0200

    Update main.py

--- 3211_python_junior/main.py
+++ 3211_python_junior/main.py
@@ -1,69 +1,40 @@
-# comments
+import random
+def game(num):
+    if num == 1:
+        return "Випав орел"
+    elif num == 0:
+        return "Випала решка"
+    else:
+        return "Сталась помилка"
 
-# testnameuser = "test"  # chaotic
-# testNameUser = 'test'  # camel case
-# TestNameUser = "test" # paskal case
-# # test-name-user =  'test' # kebab case
-# test_name_user = "test" # snake case
 
-# TYPES -- Primitives
+if __name__ == "__main__":
+    # temperature = int(input("Enter temperature outside: "))
+    # if temperature < -30:
+    #     print("It's very cold")
+    # elif 0 > temperature > -30:
+    #     print("It's cold!")
+    # elif temperature >= 0:
+    #     print("It's warm!")
+    # else:
+    #     print("Some text")
 
-# #### string (str)
-#
-# txtOne = "Test Hello World!"
-# txtTwo = 'test Hrllelel kfkfk'
-# txtThree = """test three"""
-# txtFour = '''test four'''
-#
-# ### numbers
-# # integer (int)
-# numOne = 234
-# # float (float)
-# numTwo = 23.4
-# # complex
-# numThree = 21j
-#
-# ### boolean
-# bOne = True
-# bTwo = False
-#
-# ### None
-# vNone = None
+    ### Conditional for game
+    random_number = random.randint(0, 1)
+    result = game(random_number)
+    print(f"---- {result} -----")
 
-# # OPERATION with primitives types
-# print("Type function --", type(txtOne))
-# print("UpperCase -- ", txtOne.upper())
-# print("LowerCase -- ", txtOne.lower())
-# print("Capitalize --", txtOne.capitalize())
-# print("Title -- ", txtThree.title())
-# print("Get Letter", txtOne[2])
-#
-# # number
-# print("Plus = ", numTwo + numOne)
-# print("Mimus = ", numTwo - numOne)
-# print("Multi = ", numTwo * numOne)
-# print("Dev = ", numTwo / numOne)
-# print("Up = ", numTwo**2)
-# print("Full = ", numTwo % 2)
-# print("Inc = ", numOne + 1)
-# print("Dec = ", numOne - 1)
-# print("Round = ", round(numTwo / numOne, 2))
-
-# # cond numbers
-# print(numOne >= numTwo)
-# print(numOne > numTwo)
-# print(numOne < numTwo)
-# print(numOne <= numTwo)
-# print(numOne == numTwo)
-# print(numOne != numTwo)
-
-####### --- INPUT DATA --- #######
-# template data John Patrick Doe
-print("Doe J. P.")
-firstName = input("Please enter your firstname:  ").capitalize()
-fathersName = input("Please enter your fathersname:  ").capitalize()
-lastName = input("Please enter your lastname:  ").capitalize()
-
-# print(lastName + " " + firstName[0] + ". " + fathersName[0] + ".")
-pib = f"{lastName} {firstName[0]}. {fathersName[0]}."
-print(pib)
+    while True:
+        print("1. Continue\n"
+              "2. Break")
+        ch = int(input("Enter your choice (num): "))
+        if ch == 1:
+            random_number = random.randint(0, 1)
+            result = game(random_number)
+            print(f"---- {result} -----")
+            continue
+        elif ch == 2:
+            break
+        else:
+            print("Enter incorrect chosen")
+            continue
